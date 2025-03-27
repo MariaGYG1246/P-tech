d2ba262b88c129b16123e929014cd99349b252c8
MariaGYG1246 <mpaulinagyg@gmail.com>
2025-03-27 11:16:50 -0600

    Archivo7.py
    Archivo7.py

---
+++ Rama-3/7.py
@@ -0,0 +1,10 @@
+def ami():
+    amigos = ["miranda", "ana", "brenda", "naomi", "dario"]
+    for item in amigos:
+        print(item)
+def padres():
+    pad = ["Julio", "MariaElena"]
+    for item in pad:
+        print(item)
+ami()
+padres()
